cb48f9cec9d167698851521d3a51c8c152151ca0
vinhltaz <vinhltaz@gmail.com>
2019-11-17 00:03:07 +0700

    update validation login input

--- back/controllers/user.controller.js
+++ back/controllers/user.controller.js
@@ -31,27 +31,12 @@ const register = (req, res) => {
         gender: req.body.gender
       });
 
-<<<<<<< HEAD
       user.save(err => {
         if (err) {
           console.log(err);
           return res.status(400).json({
             error: "error"
           });
-=======
-            user.save((err) => {
-                if (err) {
-
-                    return res.status(400).json({
-                        error: 'error'
-                    })
-                }
-
-                res.status(200).json({
-                    message: "Successfully signed up!"
-                })
-            })
->>>>>>> back
         }
         res.status(200).json({
           message: "Successfully signed up!"
@@ -61,42 +46,40 @@ const register = (req, res) => {
   });
 };
 
-<<<<<<< HEAD
-module.exports = {
-  register: register
-};
-=======
 const login = (req, res) => {
-    User.findOne({ "local.email": req.body.email }, (err, result) => {
-        if (err) {
-            return res.status(400).json({
-                error: 'error'
-            });
-        }
-        console.log(result)
-        if (!result) {
-            console.log('alo');
-            return res.status(400).json({
-                error: 'user khong ton tai'
-            })
-        }
-        else {
-            if (!result.authenticate(req.body.password)) {
-                return res.status(403).json({
-                    error: 'sai mat khau'
-                })
-            }
-            else {
-                res.status(200).json({
-                    message: 'ok'
-                })
-            }
-        }
-    })
-}
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+  User.findOne({ "local.email": req.body.email }, (err, result) => {
+    if (err) {
+      return res.status(400).json({
+        error: "error"
+      });
+    }
+    console.log(result);
+    if (!result) {
+      console.log("alo");
+      return res.status(400).json({
+        error: "user khong ton tai"
+      });
+    } else {
+      if (!result.authenticate(req.body.password)) {
+        return res.status(403).json({
+          error: "sai mat khau"
+        });
+      } else {
+        res.status(200).json({
+          message: "ok"
+        });
+      }
+    }
+  });
+};
 
 module.exports = {
-    register: register,
-    login: login
-}
->>>>>>> back
+  register: register,
+  login: login
+};
